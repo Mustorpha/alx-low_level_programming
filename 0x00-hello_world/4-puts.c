698139fcf541b8d35c70b63c686b7301d31343e1
mustorpha <mustorphajamiu@gmail.com>
2023-07-13 21:52:20 -0700

    new changes

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
-
-/**
- * A C puts function
-*/
-
+/
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
+}
 }
