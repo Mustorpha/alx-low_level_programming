2159734f1132e755e61f7b4a2eda6c9157bd05d3
mustorpha <mustorphajamiu@gmail.com>
2023-07-13 21:16:40 -0700

    End mandatory taks

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * A C program that prints the size of each
+ * C datatype
+*/
+
+int main(void)
+{
+
+	/* printing the size of each datatype */
+	printf("Size of a char: %lu byte(s)\n", sizeof(char));
+	printf("Size of an int: %lu byte(s)\n", sizeof(int));
+	printf("Size of an double: %lu byte(s)\n", sizeof(double));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %lu byte(s)\n", sizeof(float));
+
+	return (0);
+
+}
