f4445bef2c3f9b57e28eddea712e3c8146f03658
mustorpha <mustorphajamiu@gmail.com>
2023-07-14 00:09:44 -0700

    new changes

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,11 +5,12 @@
  *
  * Description: The program uses printf to 
  * output the string
+ *
  * Return:0
 */
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
