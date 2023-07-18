3b6df7fe07955fa82cde31128833e522e00b95c1
Mustorpha <mustorphajamiu@gmail.com>
2023-07-18 21:39:43 +0100

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,22 +1,25 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - func
+ * print_alphabet_x10 - prints the alphabetic character 10 times
  *
- * Return - 0
+ * Description: A functions that prints the whole alphabetic
+ * character into the standard output
+ * Return: 0
  */
+
 void print_alphabet_x10(void)
 {
-int b = 0;
-while (b < 10)
+int counter = 0;
+while (counter < 10)
 {
-char a = 'a';
-while (a <= 'z')
+char c = 'a';
+while (c <= 'z')
 {
-_putchar(a);
-a++;
+_putchar(c);
+c++;
 }
 _putchar('\n');
-b++;
+counter++;
 }
 }
