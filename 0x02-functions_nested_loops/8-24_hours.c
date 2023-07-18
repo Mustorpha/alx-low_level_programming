35bcdd7c131d201cfcb294ec791757ee0d8b5aa0
Mustorpha <mustorphajamiu@gmail.com>
2023-07-18 22:00:36 +0100

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * jack_bauer - every minute of the day of Jack Bauer,
+ * jack_bauer - prints every minute of the day of Jack Bauer,
  *
  * Description: A functions that prints every minute of the day of Jack Bauer,
  * starting from 00:00 to 23:59.
