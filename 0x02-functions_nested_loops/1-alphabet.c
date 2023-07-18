9bc1d12fef17a9e8b77f09c39abad2af9ae35d43
mustorpha <mustorphajamiu@gmail.com>
2023-07-18 11:04:59 -0700

    added the printing the alphate file

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - Prints the alphabets in lower case
+ *
+ * Description: A funtion that prints the known alphabets in
+ * lower case using the while loop.
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}
