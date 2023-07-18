30d607c8c912df1f25d6878a2df71f9cec3de5e1
mustorpha <mustorphajamiu@gmail.com>
2023-07-18 10:14:36 -0700

    Added the funtion loops folder

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints _putchar to the standard output
+ *
+ * Description: A function that prints the string _putchar
+ * to the standard output
+ * Return: 0
+ */
+
+int main (void)
+{
+	puts("_putchar");
+}
