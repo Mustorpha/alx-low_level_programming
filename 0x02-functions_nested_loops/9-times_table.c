3a062dffee1bb5aa8cd87438bd4a17ac95410d84
Mustorpha <mustorphajamiu@gmail.com>
2023-07-19 03:11:00 +0100

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,31 +9,30 @@
  */
 void times_table(void)
 {
-	int  x, y, z;
+	int x, y, z;
 
 	for (x = 0; x < 10; x++)
 	{
 		for (y = 0; y < 10; y++)
 		{
-			z = x * y;
+			z = y * x;
 			if (y == 0)
-				_putchar(z + '0');
-			else
 			{
-				if (x < 10)
-				{
-					_putchar(',');
-					_putchar(32);
-					_putchar(32);
-					_putchar(z + '0');
-				}
-				else
-				{
-					_putchar(',');
-					_putchar(32);
-					_putchar((z / 10) + '0');
-					_putchar((z % 10) + '0');
-				}
+				_putchar(z + '0');
+			}
+
+			if (z < 10 && y != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(z + '0');
+			} else if (z >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((z / 10) + '0');
+				_putchar((z % 10) + '0');
 			}
 		}
 		_putchar('\n');
