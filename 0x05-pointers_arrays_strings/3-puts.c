1e1975c56f9151d76bcbecfba1e2c20561d56d7a
mustorpha <mustorphajamiu@gmail.com>
2023-07-24 11:42:56 -0700

    added the 3-puts.c file

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts - prints a string to the standard output
+ * @str: pointer to the string
+ *
+ * Return: void
+ */
+
+
+void _puts(char *str)
+{
+	int j;
+
+	for (j=0; str[j]!='\0'; j++)
+	{
+		_putchar(str[j]);
+	}
+	_putchar('\n');
+}
