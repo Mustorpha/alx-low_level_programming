083e6a3f7707da54dbf6114cdd98df5671e66230
mustorpha <mustorphajamiu@gmail.com>
2023-07-24 13:39:28 -0700

    new changes

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -24,3 +24,28 @@ void print_rev(char *s)
 	}
 	_putchar('\n');
 }
+
+/**
+ * _strlen - analyse the total numbr of character in a string
+ * @s: a pointer to the string
+ *
+ * Description: the function takes a pointer to the string
+ * as parameter and loop through the number of character
+ * detected while ignoring the end of char \0
+ *
+ * Return: the number of chracter
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	return (i);
+}
