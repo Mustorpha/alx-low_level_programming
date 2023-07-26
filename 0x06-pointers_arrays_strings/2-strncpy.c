a3002a41b04cd3e3870921442b22603712e725be
mustorpha <mustorphajamiu@gmail.com>
2023-07-26 11:15:22 -0700

    new changes

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -16,17 +16,19 @@
 char *_strncpy(char *dest, char *src, int n)
 {
         int i = 0;
-        int j = 0;
 
-        while (src[j] != '\0' && j < n)
+        while (src[i] != '\0' && i < n)
         {
-                dest[i] = src[j];
+                dest[i] = src[i];
 
                 i++;
-                j++;
         }
 
-	dest[j] = '\0';
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
 
         return dest;
 }
