17de708287bccc45443e8eb4a0d1c222ac4f847e
mustorpha <mustorphajamiu@gmail.com>
2023-07-26 10:33:20 -0700

    update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,21 +11,23 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int i = 0;
-	int j = 0;
+	int i;
+	int j;
+
+	i = 0;
 
 	while (dest[i] != '\0')
 	{
 		i++;
 	}
 
+	j = 0;
+
 	while (src[j])
 	{
 		dest[i] = src[j];
-
 		i++;	
 		j++;
 	}
-
 	return (dest);
 }
