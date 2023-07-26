c1c33da8b41221e874fa30c1b4c5b2ebcc21ed2e
Mustorpha <mustorphajamiu@gmail.com>
2023-07-26 19:31:40 +0100

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,15 +1,16 @@
 #include "main.h"
 
 /**
- * _strcmp -  function that compares two strings.
- * @s1: primera cadena.
- * @s2: segunda cadena.
- * Return: 0.
+ * _strcmp -  function that compares two strings
+ * @s1: the first string
+ * @s2: the second string
+ * 
+ * Return: the value from comparism
  */
 
 int _strcmp(char *s1, char *s2)
 {
-	int res = 0;
+	int val = 0;
 
 	while (*s1 == *s2 && *s1 != '\0')
 	{
@@ -17,7 +18,7 @@ int _strcmp(char *s1, char *s2)
 		s2++;
 	}
 	if (s1 != s2)
-		res = *s1 - *s2;
+		val = *s1 - *s2;
 
-	return (res);
+	return (val);
 }
