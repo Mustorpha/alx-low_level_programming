f206b30520102accd349102418e940abc82b8088
Mustorpha <mustorphajamiu@gmail.com>
2023-07-27 22:59:17 +0100

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,20 +5,20 @@
  * _strcmp - function that compares two strings
  * @s1: the first string
  * @s2: the second string
- * 
  * Return: the value from comparism
  */
 
 int _strcmp(char *s1, char *s2)
 {
-	int count, cmpVal;
+	int val = 0;
 
-	counter = 0;
-	while (s1[count] == s2[count] && s1[count] != '\0')
+	while (*s1 == *s2 && *s1 != '\0')
 	{
-		count++;
+		s1++;
+		s2++;
 	}
+	if (s1 != s2)
+		val = *s1 - *s2;
 
-	cmpVal = s1[count] - s2[count];
-	return (cmpVal);
+	return (val);
 }
