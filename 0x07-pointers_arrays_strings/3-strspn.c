646527a69ce37899e0db11e94b2590c241304675
Mustorpha <mustorphajamiu@gmail.com>
2023-07-31 20:07:46 +0100

    Update 3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,27 +1,28 @@
 #include "main.h"
 
 /**
- * _strspn - gets the length of a prefix substring.
- * @s: initial segment.
- * @accept: accepted bytes.
- * Return: the number of accepted bytes.
+ * _strspn - gets the length of a prefix substring
+ * @s: initial segment
+ * @accept: accepted bytes
+ * Return: the number of accepted bytes
  */
+
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i, j, bool;
+	unsigned int i, j, flag;
 
 	for (i = 0; *(s + i) != '\0'; i++)
 	{
-		bool = 1;
+		flag = 1;
 		for (j = 0; *(accept + j) != '\0'; j++)
 		{
 			if (*(s + i) == *(accept + j))
 			{
-				bool = 0;
+				flag = 0;
 				break;
 			}
 		}
-		if (bool == 1)
+		if (flag == 1)
 			break;
 	}
 	return (i);
