97b25d3977c361968f18f122251ada7d9012d363
mustorpha <mustorphajamiu@gmail.com>
2023-08-04 08:23:32 -0700

    added the 1-agrs.c file and modify 0-whatsmyname.c file

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints the integer stored in argc by the CLI
+ * @argc: number of arguments
+ * @argv: array of pointers to the arguments
+ * Return: 0
+ */
+
+int main(int argc, char __attribute__((__unused__)) **argv)
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
