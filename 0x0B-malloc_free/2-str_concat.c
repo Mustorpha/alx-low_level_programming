aade47550f9fefb4b8a6367ca4452f706f518407
mustorpha <mustorphajamiu@gmail.com>
2023-08-07 06:08:05 -0700

    new changes

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,52 +11,41 @@
 
 char *str_concat(char *s1, char *s2)
 {
-    int len, i, j;
+	char *str;
+	int len, i, j = 0;
 
-    if (s1 == NULL)
-    {
-        *s1 = "";
-    }
-    else
-    {
-        for (i = 0; s1[i] != '\0'; i++)
-        {
-            len++;
-        }
-    }
-    if (s2 == NULL)
-    {
-        *s2 = "";
-    }
-    else
-    {
-        for (i = 0; s2[i] != '\0'; i++)
-        {
-            len++;
-        }
-    }
-    char *str = (char *)malloc((++len) * sizeof(char));
-    
-    if (str == NULL)
-    {
-        free(str);
-        return NULL;
-    }
-    i = 0;
-    j = 0;
+	if (s1 == NULL)
+	{
+		*s1 = "";
+	}
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		len++;
+	}
+	if (s2 == NULL)
+	{
+		*s2 = "";
+	}
+	for (i = 0; s2[i] != '\0'; i++)
+	{
+		len++;
+	}
+	str = malloc((++len) * sizeof(char));
 
-    while(s1[i])
-    {
-        str[i] = s1[i];
-        i++;
-    }
-
-    while(s2[j])
-    {
-        str[i] = s2[j];
-        i++;
-        j++;
-    }
-
-    return (str);
+	if (str == NULL)
+	{
+		free(str);
+		return (NULL);
+	}
+	for (i = 0; i < len; i++)
+	{
+		str[i] = s1[i];
+	}
+	while (s2[j])
+	{
+		str[i] = s2[j];
+		i++;
+		j++;
+	}
+	return (str);
 }
