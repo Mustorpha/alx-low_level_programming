3d5b35e07b99974f6a7c98b6c2dcdae3dd2b10f7
mustorpha <mustorphajamiu@gmail.com>
2023-08-07 14:47:22 -0700

    new changes

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -30,11 +30,11 @@ int **alloc_grid(int width, int height)
 		arr[i] = (int *)calloc(width, sizeof(int));
 		if (arr[i] == NULL)
 		{
-			for (i--; i >= 0; i++)
+			free(arr);
+			for (i--; i >= 0; i--)
 			{
 				free(arr[i]);
 			}
-			free(arr);
 			return (NULL);
 		}
 	}
