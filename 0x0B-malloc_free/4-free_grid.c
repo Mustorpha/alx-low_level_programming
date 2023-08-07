fbbec0da1c012b695c5405fba3cd73703bb28830
mustorpha <mustorphajamiu@gmail.com>
2023-08-07 14:54:43 -0700

    new changes

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -9,16 +9,16 @@
  * Return: void (success)
 */
 
-void free_grid (int **grid, int height)
+void free_grid(int **grid, int height)
 {
-    int i;
+	int i;
 
-    if (grid && height)
-    {
-        for (i = 0; i < height; i++)
-        {
-            free(grid[i]);
-        }
-        free(grid);
-    }
+	if (grid && height)
+	{
+		for (i = 0; i < height; i++)
+		{
+			free(grid[i]);
+		}
+		free(grid);
+	}
 }
