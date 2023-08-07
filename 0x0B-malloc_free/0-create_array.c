86568097535aab7c158009c13f2fc3c255781707
mustorpha <mustorphajamiu@gmail.com>
2023-08-07 05:18:59 -0700

    new chagnes

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,13 +12,14 @@
 
 char *create_array(unsigned int size, char c)
 {
+	char *arr;
 	unsigned int i;
 
 	if (size == 0)
 	{
 		return (NULL);
 	}
-	char *arr = malloc(size * sizeof(c));
+	*arr = malloc(size * sizeof(c));
 
 	if (arr == NULL)
 	{
