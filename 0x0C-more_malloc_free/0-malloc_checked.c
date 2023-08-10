6f67e13a194dff08edf8016b35905cd35200abb3
mustorpha <mustorphajamiu@gmail.com>
2023-08-10 13:52:59 -0700

    added 0-malloc_checked.c file

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * malloc_checked - allocates memory space
+ * @b: memory size
+ * Return: a pointer to the memory or exit program
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	int *mem = malloc(b);
+
+	if (mem == NULL)
+	{
+		exit(98);
+	}
+	return (98);
+}
