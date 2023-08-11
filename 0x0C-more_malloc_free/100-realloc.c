56b5883620f4abb842208403642d8f53d3b9142c
mustorpha <mustorphajamiu@gmail.com>
2023-08-10 21:52:27 -0700

    new changes

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -11,30 +11,31 @@
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
+	char *nptr;
 	unsigned int i;
-	char *p;
 
 	if (new_size == old_size)
 		return (ptr);
-
-	if (ptr == NULL)
-		ptr = malloc(new_size);
-
-	if (new_size == 0 && ptr != NULL)
+	if ((new_size == 0) && (ptr != NULL))
 	{
 		free(ptr);
 		return (NULL);
 	}
-
+	if (ptr == NULL)
+	{
+		nptr = malloc(new_size);
+		if (nptr == NULL)
+			return (NULL);
+	}
 	if (new_size > old_size && (ptr != NULL))
 	{
-		p = malloc(new_size);
-		if (p == NULL)
-			return (p);
+		nptr = malloc(new_size);
+		if (nptr == NULL)
+			return (nptr);
 		for (i = 0; i < old_size; i++)
-			p[i] = *((char *)ptr + 1);
+			nptr[i] = *((char *)ptr + 1);
 		free(ptr);
 	}
 
-	return (p);
+	return (nptr);
 }
