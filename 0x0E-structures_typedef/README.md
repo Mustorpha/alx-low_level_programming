a0fec1055bfe6fb7519a885e21742601e295cb5c
mustorpha <mustorphajamiu@gmail.com>
2023-08-14 08:14:20 -0700

    first commit

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,2 @@
+# Structures and type definition in C Language
+This is a collection of my task on <code>struct</code> and <code>typdef</code> keyword in C
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a structure for dog type
+ * @name: the dog name
+ * @age: the dog age
+ * @owner: the owners name
+ *
+ * Description: The structure holds necessary information about a dog.
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
