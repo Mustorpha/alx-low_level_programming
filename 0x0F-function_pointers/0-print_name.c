cb36b898055546979bb510072964767c58d975fe
mustorpha <mustorphajamiu@gmail.com>
2023-08-16 02:11:48 -0700

    new changes

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name
+ * @name: the name to be printed
+ * @f: the printing function
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,4 @@
+# Functions Pointers in C Language
+This is a collection of tasks on poniters to functions in C
+and how to use them. What function pointers holds and what
+they point to in the virtual memory
