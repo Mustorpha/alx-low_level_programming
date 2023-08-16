286a4b4a972199effda3a8574639d8aca7ac5384
mustorpha <mustorphajamiu@gmail.com>
2023-08-16 13:14:35 -0700

    Upload the advanced tasks

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - a program that prubts the opcodes of its
+ * own main function
+ * @argc: total number of argument received
+ * @argv: an array of pointers to arguments
+ * Return: 0 (success)
+ */
+int main(int argc, char **argv)
+{
+	int i;
+	char *ops;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	i = atoi(argv[1]);
+
+	if (i < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	ops = (char *)&main;
+
+	while (i)
+	{
+		if (i == 1)
+			printf("%.2hhx\n", *(ops));
+		printf("%.2hhx ", *(ops));
+		ops++;
+		i--;
+	}
+}
