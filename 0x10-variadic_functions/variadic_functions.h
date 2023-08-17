056915734e7af8ae5a82ab97ce64b8bd881e6551
Mustorpha <mustorphajamiu@gmail.com>
2023-08-17 14:50:46 +0100

    Update variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,7 +1,9 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _VARIADIC_FUNCTIONS_
+#define _VARIADIC_FUNCTIONS_
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 
 #endif
