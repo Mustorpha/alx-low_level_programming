0ed8862f7d48d0093ba7d15f6c08a9e87aeda1c1
mustorpha <mustorphajamiu@gmail.com>
2023-08-17 05:50:40 -0700

    First commit

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,24 @@
+#include <stdarg.h>
+
+/**
+ * sum_them_all - sum up all argument received
+ * @n: the argument count
+ * Return: sum
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i;
+	int sum = 0;
+	va_list args_input;
+
+	if (n == 0)
+		return (0);
+	va_start(args_input, n);
+	for (i = 0; i < n; i++)
+	{
+		sum += a_args(args_input, int);
+	}
+	var_end(args_input);
+
+	return (sum);
+}
