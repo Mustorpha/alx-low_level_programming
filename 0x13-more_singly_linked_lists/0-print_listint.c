61bf8173faa4f6d591ccdb4f4c6c8ebdd8bb196f
mustorpha <mustorphajamiu@gmail.com>
2023-08-28 23:58:16 -0700

    new changes

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -3,20 +3,20 @@
 
 /**
  * print_listint - prints elements of a singly linked list
- * @h: a pointer to the list
+ * @h: pointer to a list
  * Return: number of nodes
  **/
 size_t print_listint(const listint_t *h)
 {
-	const listint_t *hCopy;
+	const listint_t *h_copy;
 	unsigned int number_of_nodes = 0;
 
-	hCopy = h;
-	while (hCopy)
+	h_copy = h;
+	while (h_copy)
 	{
-		printf("%d\n", hCopy->n);
+		printf("%d\n", h_copy->n);
 		number_of_nodes++;
-		hCopy = hCopy->next;
+		h_copy = tp->next;
 	}
 	return (number_of_nodes);
 }
